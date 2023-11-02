29547e989e0378979aac5e94a1563ba80ce983a8
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2023-11-02 16:34:12 +0200

    add user routing as lazy loaded module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)}
+  { path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+  }
+
 ];
 
 @NgModule({
