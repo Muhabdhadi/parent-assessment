837ff0c94dd448679abe04ae47ef746df56caba1
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2023-11-02 20:04:34 +0200

    call get users and display

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../user.service";
+import {UsersResponseInterface} from "./users-response.interface";
+import {UserInterface} from "./user.interface";
+import {ToasterService} from "../../shared/toasts/toaster.service";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.scss']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+    page = 0;
+    per_page = 5;
+    users: UserInterface[] = []
 
+    constructor(private userService: UserService, private toasterService: ToasterService) {
+    }
+
+    ngOnInit() {
+        this.getUsers();
+    }
+
+    getUsers() {
+        this.userService.getUsers({page: this.page, per_page: this.per_page})
+            .subscribe({
+            next: (res) => this.handleUsersSuccess(res),
+
+            error: () => this.handleUsersError()
+        });
+    }
+
+    handleUsersSuccess(res: UsersResponseInterface) {
+        this.users = res.data;
+    }
+
+    handleUsersError() {
+        this.toasterService.show('Error while getting users', {
+            className: 'bg-danger text-light'
+        });
+    }
 }
