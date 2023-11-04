4aa6c9c25d0e52e037d92e5bad7078a636e2ca25
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2023-11-04 21:00:56 +0200

    add auth guard add logout functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {authGuard} from "./auth/auth.guard";
 
 const routes: Routes = [
-  { path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
-  },
+    {path: '', redirectTo: 'user', pathMatch: 'full'},
 
-  {
-    path: 'user',
-    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
-  }
+    {
+        path: 'login',
+        loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+    },
+
+    {
+        path: 'user',
+        loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+        canActivate: [authGuard]
+    }
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
